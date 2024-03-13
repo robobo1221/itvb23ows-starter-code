e900d3da1256c6ff226a35ebb821ea5bf836c346
robobo1221 <robobo1221@gmail.com>
2024-03-13 15:44:55 +0100

    Rearranged filestructure

--- tests/utilTest.php
+++ tests/utilTest.php
@@ -5,7 +5,43 @@ use Util\BoardUtil;
 include_once "src/util.php";
 
 class UtilTest extends TestCase {
-    //public function
+    public function testAvailableTiles(): void {
+        $hand = ["Q" => 1, "B" => 0, "S" => 2, "A" => 2, "G" => 0]; // arrange
+        $availableTiles = BoardUtil::getAvailableTiles($hand);      // act
+
+        $this->assertSame(["Q", "S", "A"], $availableTiles);        // assert
+    }
+
+    public function testFirstAvailablePlays(): void {
+        $hand = ["Q" => 1, "B" => 2, "S" => 2, "A" => 3, "G" => 3];             // arrange
+        $board = [];
+        $player = 0;
+
+        $availablePlays = BoardUtil::getAvailablePlays($hand, $board, $player); // act
+        
+        $this->assertSame(["0,0"], $availablePlays);                            // assert
+    }
+
+    public function testAvailablePlays(): void {
+        $hand = ["Q" => 0, "B" => 2, "S" => 2, "A" => 3, "G" => 3];             // arrange
+        // Player 0 played Q on position 0,0, then player 1 played S on position 0,1.
+        $board = ["0,0" => [[0, "Q"]], "0,1" => [[1, "S"]]];
+        $player = 0;
+
+        $availablePlays = BoardUtil::getAvailablePlays($hand, $board, $player); // act
+        
+        $this->assertSame(["0,-1", "-1,0", "1,-1"], $availablePlays);           // assert
+    }
+
+    public function testAvailableFrom(): void {
+        // Testing on player 1. Player 1 has only one position on the board.
+        $board = ["0,0" => [[0, "Q"]], "0,1" => [[1, "Q"]]];            // arrange
+        $player = 1;
+
+        $availableFroms = BoardUtil::getAvailableFrom($board, $player); // act
+
+        $this->assertSame(["0,1"], $availableFroms);                    // assert
+    }
 
     public function testGrasshopperDiagonalDownValidMove() {
         // arrange
